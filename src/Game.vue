eb972eea119463ef2bf16d7f13688d980c77a577
toom94 <36904439+toom94@users.noreply.github.com>
2022-03-06 20:20:58 +0100

    Update Game.vue

--- src/Game.vue
+++ src/Game.vue
@@ -75,7 +75,7 @@ function completeRow() {
     const guess = currentRow.map((tile) => tile.letter).join('')
     if (!allWords.includes(guess) && guess !== answer) {
       shake()
-      showMessage(`Not in word list`)
+      showMessage(`Nie ma na liście`)
       return
     }
 
@@ -179,13 +179,8 @@ function genResultGrid() {
     </div>
   </Transition>
   <header>
-    <h1>VVORDLE</h1>
-    <a
-      id="source-link"
-      href="https://github.com/yyx990803/vue-wordle"
-      target="_blank"
-      >Source</a
-    >
+    <h1>Literalnie Pauli</h1>
+
   </header>
   <div id="board">
     <div
