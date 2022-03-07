5cd76504dc72e0ddc2a6f10f89f0bc3a51f9dc11
toom94 <36904439+toom94@users.noreply.github.com>
2022-03-07 01:51:53 +0100

    Update Game.vue

--- src/Game.vue
+++ src/Game.vue
@@ -43,7 +43,7 @@ onUnmounted(() => {
 
 function onKey(key: string) {
   if (!allowInput) return
-  if (/^[a-zA-Z]$/.test(key)) {
+  if (/^[a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]$/.test(key)) {
     fillTile(key.toLowerCase())
   } else if (key === 'Backspace') {
     clearTile()
